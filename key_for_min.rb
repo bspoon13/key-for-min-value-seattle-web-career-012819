fd80c9e20f4d7051e175084f6e10cbdf9cdf6a73
Billy Witherspoon <wwither@gmail.com>
2019-01-25 02:20:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   initial_number = 0
   min_number = nil
-  #min_name = nil
+  min_name = nil
   name_hash.each do |name, number|
     if initial_number == 0
       initial_number += 1
